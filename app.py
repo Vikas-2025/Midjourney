826a032333912fea3655b35ad845ed18f79f2d65
DanitonAI <Daniton@users.noreply.huggingface.co>
2023-01-26 15:20:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import torch
 import numpy as np
 
 model_id = "prompthero/openjourney"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+
 
 def generate_image(prompt):
     image = pipe(prompt).images[0]
