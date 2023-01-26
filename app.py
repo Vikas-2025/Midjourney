c4d1084214c03eed83eeab943295adb46372cc6f
DanitonAI <Daniton@users.noreply.huggingface.co>
2023-01-26 15:15:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import gradio as gr
 from diffusers import StableDiffusionPipeline
 import torch
-from PIL import Image
+import numpy as np
 
 model_id = "prompthero/openjourney"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
 def generate_image(prompt):
     image = pipe(prompt).images[0]
-    image = Image.fromarray(image.numpy())
+    image = np.asarray(image)
     return image
 
-iface = gr.Interface(generate_image, gr.inputs.Textbox(label="Enter a prompt"), gr.outputs.Image())
-iface.launch() 
+iface = gr.Interface(generate_image, gr.inputs.Textbox(label="Enter a prompt"), gr.outputs.Image(type='numpy'))
+iface.launch()
